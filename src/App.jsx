fe092319a04f4308628bca5e8ca8fc496ab88858
Josh Frank <eurosewusa@gmail.com>
2025-08-22 16:37:44 -0400

    adding useSwr directly pt3

--- src/App.jsx
+++ src/App.jsx
@@ -71,11 +71,11 @@ function App() {
   };
 
   // const { mutate: saveHistogram } = useHistogram( state.textInput )
-  async function saveHisto( url, { corpus } ) {
+  async function saveHisto( url, { arg } ) {
     return fetch( url, {
       method: 'POST',
-      body: JSON.stringify( { histogram: { corpus } } )
-    } ).then( response => response.json() )
+      body: JSON.stringify( { histogram: { arg } } )
+    } ).then( res => res.json() )
   }
   const {
     trigger,
